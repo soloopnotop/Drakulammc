925748d7521879cadbce1e1754be7b88fa32d81e
solo <itzsoloplayzbusiness@gmail.com>
2025-01-16 12:09:56 +0500

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,129 @@
+const config = {
+    serverInfo: {
+        serverLogoImageFileName: "logo.png", 
+        serverName: "Drakula Mc", 
+        serverIp: "play.drakulamc.fun", 
+        
+    },
+    
+  
+   
+}
+
+const navbar = document.querySelector(".navbar");
+const navbarLinks = document.querySelector(".links");
+const hamburger = document.querySelector(".hamburger");
+
+hamburger.addEventListener("click", () => {
+    navbar.classList.toggle("active");
+    navbarLinks.classList.toggle("active");
+})
+
+
+const accordionItemHeaders = document.querySelectorAll(".accordion-item-header");
+
+accordionItemHeaders.forEach(accordionItemHeader => {
+    accordionItemHeader.addEventListener("click", () => {
+        accordionItemHeader.classList.toggle("active");
+        const accordionItemBody = accordionItemHeader.nextElementSibling;
+
+        if(accordionItemHeader.classList.contains("active")) accordionItemBody.style.maxHeight = accordionItemBody.scrollHeight + "px";
+        else accordionItemBody.style.maxHeight = "0px";
+    });
+});
+
+
+const serverName = document.querySelector(".server-name");
+const serverLogo = document.querySelector(".logo-img");
+
+const serverIp = document.querySelector(".minecraft-server-ip");
+const serverLogoHeader = document.querySelector(".logo-img-header");
+const discordOnlineUsers = document.querySelector(".discord-online-users");
+const minecraftOnlinePlayers = document.querySelector(".minecraft-online-players");
+
+const contactForm = document.querySelector(".contact-form");
+const inputWithLocationAfterSubmit = document.querySelector(".location-after-submit");
+
+const getDiscordOnlineUsers = async () => {
+    try {
+        const discordServerId = config.serverInfo.discordServerID;
+
+        const apiWidgetUrl = `https://discord.com/api/guilds/1279333791239766048/widget.json`;
+        let response = await fetch(apiWidgetUrl);
+        let data = await response.json();
+
+        if(!data.presence_count) return "None";
+        else return (await data.presence_count);
+    } catch (e) {
+        return "None";
+    }
+}
+
+const getMinecraftOnlinePlayer = async () => {
+    try {
+        const serverIp = config.serverInfo.serverIp;
+
+        const apiUrl = `https://api.mcsrvstat.us/2/play.drakulamc.fun`;
+        let response = await fetch(apiUrl);
+        let data = await response.json();
+
+        return data.players.online;
+    } catch (e) {
+        console.log(e);
+        return "None";
+    }
+}
+
+const copyIp = () => {
+    const copyIpButton = document.querySelector(".copy-ip");
+    const copyIpAlert = document.querySelector(".ip-copied");
+
+    copyIpButton.addEventListener("click", () => {
+        try {
+            navigator.clipboard.writeText(config.serverInfo.serverIp);
+    
+            copyIpAlert.classList.add("active");
+
+            setTimeout(() => {
+                copyIpAlert.classList.remove("active");
+            }, 5000);
+        } catch (e) {
+            console.log(e);
+            copyIpAlert.innerHTML = "An error has occurred!";
+            copyIpAlert.classList.add("active");
+            copyIpAlert.classList.add("error");
+
+            setTimeout(() => {
+                copyIpAlert.classList.remove("active");
+                copyIpAlert.classList.remove("error");
+            }, 5000);
+        }
+    })
+}
+const setDataFromConfigToHtml = async () => {
+   
+    serverName.innerHTML = config.serverInfo.serverName;
+    serverLogo.src = `images/` + config.serverInfo.serverLogoImageFileName;
+
+   
+    serverIp.innerHTML = config.serverInfo.serverIp;
+
+    let locationPathname = location.pathname;
+
+    if(locationPathname == "/" || locationPathname.includes("index")) {
+        copyIp();
+      
+        serverLogoHeader.src = `images/` + config.serverInfo.serverLogoImageFileName;
+        discordOnlineUsers.innerHTML = await getDiscordOnlineUsers();
+        minecraftOnlinePlayers.innerHTML = await getMinecraftOnlinePlayer();
+    } 
+  
+    
+   else if(locationPathname.includes("contact")) {
+        contactForm.action = `https://formsubmit.co/notgamerpratham@gmail.com`;
+        discordOnlineUsers.innerHTML = await getDiscordOnlineUsers();
+        inputWithLocationAfterSubmit.value = location.href;
+    }
+}
+
+setDataFromConfigToHtml();
